a005018e335b4305305f648fff81e5aad16b3f40
Meriadec Pillet <meriadec.pillet@gmail.com>
2022-12-20 23:55:10 +0100

    Add some colors, start layouting

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,19 @@
 export default function Home() {
-  return <div className="font-bold">Home</div>;
+  return (
+    <div className="p-8">
+      <div className="flex justify-between items-start">
+        <div className="uppercase text-blue-2 tracking-widest select-none">{"Astroquirks"}</div>
+        <button className="border-2 border-orange-1 rounded-full px-8 py-4 font-bold hover:bg-orange-1 hover:text-blue-1">
+          {"Launch App"}
+        </button>
+      </div>
+      <div>
+        <div className="uppercase text-[100px] font-bold leading-none text-orange-3 pt-[100px] pl-[100px] select-none">
+          {"Profit-sharing"}
+          <br />
+          <span className="text-orange-4">{"is caring"}</span>
+        </div>
+      </div>
+    </div>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./pages/**/*.tsx", "./components/**/*.tsx"],
   theme: {
-    extend: {},
+    colors: {
+      "blue-1": "hsl(222, 61%, 15%)",
+      "blue-2": "hsl(202, 46%, 59%)",
+      "orange-1": "hsl(42, 36%, 65%)",
+      "orange-2": "hsl(36, 24%, 53%)",
+      "orange-3": "hsl(43, 56%, 68%)",
+      "orange-4": "hsl(7, 62%, 68%)",
+    },
   },
   plugins: [],
 };
