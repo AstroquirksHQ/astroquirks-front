2db0e20e2f38b0b8762c63a3dc7b278763e8fe3e
Meriadec Pillet <meriadec.pillet@gmail.com>
2022-12-20 23:21:11 +0100

    Fix favicon path - #1

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,15 +3,17 @@ import Head from "next/head";
 
 import "../styles/globals.css";
 
+const prefix = process.env.NODE_ENV === "production" ? "/astroquirks-front" : "";
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
         <title>{"Astroquirks"}</title>
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="apple-touch-icon" sizes="180x180" href={`${prefix}/apple-touch-icon.png`} />
+        <link rel="icon" type="image/png" sizes="32x32" href={`${prefix}/favicon-32x32.png`} />
+        <link rel="icon" type="image/png" sizes="16x16" href={`${prefix}/favicon-16x16.png`} />
+        <link rel="manifest" href={`${prefix}/site.webmanifest`} />
         <meta name="theme-color" content="#000000" />
       </Head>
       <Component {...pageProps} />
