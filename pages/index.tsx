08d1b5f8a7a789b2c7f269265b9919f2202cb193
Meriadec Pillet <meriadec.pillet@gmail.com>
2022-12-20 22:02:21 +0100

    Adjust start, add CI

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
-  return (
-    <div>
-      <div className="text-4xl font-bold p-8">profit-sharing is caring.</div>
-      <Image src="/img/chest.png" draggable="false" width={256} height={256} alt="chest" />
-    </div>
-  );
+  return <div className="font-bold">Home</div>;
 }
