e2b9e3f86e868a27602068dabfe4061c0072cfe9
Meriadec Pillet <meriadec.pillet@gmail.com>
2023-01-31 17:30:14 +0100

    Revert "hear (#21)"
    This reverts commit bc297f14321963431037b089163e68ced3b201ce.

--- components/InteractiveHeart.tsx
+++ components/InteractiveHeart.tsx
@@ -1,59 +1,75 @@
 import { motion } from "framer-motion";
 import Image from "next/image";
-import { useEffect, useRef, useState } from "react";
+import { ReactNode, useRef, useState } from "react";
+import { FaHeart } from "react-icons/fa";
 
 const InteractiveHeart = () => {
-  const [refOffset, setRefOffset] = useState<{ x: number; y: number } | null>(null);
-  const [offset, setOffset] = useState<{ x: number; y: number }>({ x: 0, y: 0 });
-  const ref = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    const measure = () => {
-      if (!ref.current) return;
-      const rect = ref.current.getBoundingClientRect();
-      const refOffset = { x: Math.round(rect.left), y: Math.round(rect.top) };
-      setRefOffset(refOffset);
-    };
-    measure();
-    window.addEventListener("resize", measure);
-    return () => {
-      window.removeEventListener("resize", measure);
-    };
-  }, [setRefOffset]);
-
-  useEffect(() => {
-    if (!refOffset) return;
-    const adjustPosition = (e: MouseEvent) => {
-      if (e.pageY > 250 && e.pageY < 650) {
-        setOffset({ x: -(refOffset.x - e.pageX) - 30, y: -(refOffset.y - e.pageY) - 30 });
-      } else {
-        setOffset({ x: 0, y: 0 });
-      }
-    };
-    const resetPosition = () => {
-      setOffset({ x: 0, y: 0 });
-    };
-    document.addEventListener("mousemove", adjustPosition);
-    document.addEventListener("mouseleave", resetPosition);
-    return () => {
-      document.removeEventListener("mousemove", adjustPosition);
-      document.removeEventListener("mouseleave", resetPosition);
-    };
-  }, [refOffset, setOffset]);
-
+  const constraintsRef = useRef(null);
+  const [isDragging, setDragging] = useState(false);
+  const onDragStart = () => setDragging(true);
+  const onDragEnd = () => setDragging(false);
   return (
-    <div ref={ref}>
-      <motion.div animate={offset}>
+    <motion.div ref={constraintsRef}>
+      <motion.div
+        drag
+        dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }}
+        dragTransition={{ bounceStiffness: 300, bounceDamping: 15 }}
+        whileDrag={{ scale: 1.5 }}
+        className="cursor-grab"
+        onDragStart={onDragStart}
+        onDragEnd={onDragEnd}
+      >
+        {isDragging && (
+          <div className="absolute top-0 left-0 right-0 bottom-0 flex items-center justify-center">
+            <BouncingSymbols>
+              <FaHeart className="text-[10px] text-[red] absolute top-0 left-0" />
+            </BouncingSymbols>
+          </div>
+        )}
         <div className="animate-orbit w-[40px] h-[40px] lg:w-[60px] lg:h-[60px]">
           <Image
             src="/img/heart.svg"
-            className="animate-orbit-reverse w-[30px] h-[30px] lg:w-[40px] lg:h-[40px]"
+            className="animate-orbit-reverse w-[30px] h-[30px] lg:w-[40px] lg:h-[40px] pointer-events-none"
             alt="heart"
             width={40}
             height={40}
           />
         </div>
       </motion.div>
+    </motion.div>
+  );
+};
+
+const BouncingSymbols = ({ children }: { children: ReactNode }) => {
+  return (
+    <div className="relative">
+      {Array(8)
+        .fill(undefined)
+        .map((_, i) => {
+          const col = 20;
+          const row = 20;
+          const x = (i % 4) * col - 20;
+          const y = Math.ceil(i / 4) * row + 20;
+          return (
+            <motion.div
+              key={i}
+              animate={{
+                x,
+                y: -y,
+                opacity: [0, 0.5, 0],
+                scale: [0, 1, 0.2],
+                rotate: `${Math.round(Math.random() * 40 - 40)}deg`,
+              }}
+              style={{ originX: 0.5, originY: 0.5 }}
+              transition={{
+                repeat: Infinity,
+                duration: 0.5 + Math.random(),
+              }}
+            >
+              {children}
+            </motion.div>
+          );
+        })}
     </div>
   );
 };
