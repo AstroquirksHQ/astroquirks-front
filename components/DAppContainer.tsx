f3c237eab9ca18479451ca0c6ce0b8ef2b48ec95
Meriadec Pillet <meriadec.pillet@gmail.com>
2023-02-04 01:25:49 +0100

    Fix layout

--- components/DAppContainer.tsx
+++ components/DAppContainer.tsx
@@ -1,8 +1,8 @@
 import { ReactNode } from "react";
 
 const DappContainer = ({ children }: { children: ReactNode }) => (
-  <div className="h-screen flex flex-col p-2 sm:p-8">
-    <div className="border border-blue-2 border-opacity-20 rounded-lg bg-blue-2 bg-opacity-5 p-3 glass shadow-md flex-grow flex flex-col">
+  <div className="h-screen flex flex-col items-center justify-center p-2 sm:p-8">
+    <div className="border border-blue-2 border-opacity-20 rounded-lg bg-blue-2 bg-opacity-5 p-3 glass shadow-md flex flex-col max-w-[730px] max-h-[400px]">
       {children}
     </div>
   </div>
