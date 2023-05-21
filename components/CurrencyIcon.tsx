711d18946aca677b3c0eb6f06e423b97546ab941
Meriadec Pillet <meriadec.pillet@gmail.com>
2023-05-21 18:27:27 +0200

    Add HUAHUA icon

--- components/CurrencyIcon.tsx
+++ components/CurrencyIcon.tsx
@@ -5,6 +5,7 @@ const iconsByTicker: Record<string, string> = {
   STARS: "https://cdn.jsdelivr.net/gh/cosmos/chain-registry/stargaze/images/stars.png",
   MARS: "https://cdn.jsdelivr.net/gh/cosmos/chain-registry/mars/images/mars-token.png",
   JUNO: "https://cdn.jsdelivr.net/gh/cosmos/chain-registry/juno/images/juno.png",
+  HUAHUA: "https://cdn.jsdelivr.net/gh/cosmos/chain-registry/chihuahua/images/huahua.png",
 };
 
 type Props = DetailedHTMLProps<ImgHTMLAttributes<HTMLImageElement>, HTMLImageElement> & {
